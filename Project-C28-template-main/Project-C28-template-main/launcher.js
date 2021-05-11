2fe9f7c03b1a59308d24cfd7756f53c70eadafdc
GauravBachate <74489270+GauravBachate@users.noreply.github.com>
2021-05-11 18:05:50 +0530

    Add files via upload

---
+++ Project-C28-template-main/Project-C28-template-main/launcher.js
@@ -0,0 +1,27 @@
+class launcher {
+    constructor(bodyA,pointB){
+        var option = {
+      bodyA:bodyA,
+      pointB,pointB,
+      stiffness:0.04,
+      length:10
+        } 
+        this.pointB = pointB
+        this.rope=Constraint.create (option)
+        World.add (world,this.rope)
+    }
+    fly(){
+        this.rope.bodyA = null
+    }
+    attach(body){
+        this.rope.bodyA = body
+    }
+    display(){
+     if (this.rope.bodyA){
+         strokeWeight (7)
+         line (this.rope.bodyA.position.x,this.rope.bodyA.position.y,this.pointB.x,this.pointB.y)
+     }
+    }
+
+    
+}
